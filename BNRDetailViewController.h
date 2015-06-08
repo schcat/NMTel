0b90d78b91ec5009fe75be736097997e8c936d91
Zhixue Li <939851583@qq.com>
2015-06-09 06:09:09 +0800

    Create BNRDetailViewController.h

---
+++ BNRDetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  BNRDetailViewController.h
+//  0526NMTel
+//
+//  Created by Zhixue Li on 5/30/15.
+//  Copyright (c) 2015 Zhixue Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class BNRItem;
+
+@interface BNRDetailViewController : UIViewController
+
+@property (nonatomic, strong) BNRItem *item;
+
+@end
