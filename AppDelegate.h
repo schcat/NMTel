0e9c9cb1f2f18dd88567fd5bfdcefa0901e80d64
Zhixue Li <939851583@qq.com>
2015-06-09 06:07:55 +0800

    Create AppDelegate.h

---
+++ AppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.h
+//  0526NMTel
+//
+//  Created by Zhixue Li on 5/26/15.
+//  Copyright (c) 2015 Zhixue Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UINavigationController *navController1;
+@property (strong, nonatomic) UINavigationController *navController2;
+
+@end
