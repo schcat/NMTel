aab296cd9a653cba0ee4956be642e47f4ec07344
Zhixue Li <939851583@qq.com>
2015-06-09 06:11:23 +0800

    Create BNRItemsViewController2.h

---
+++ BNRItemsViewController2.h
@@ -0,0 +1,14 @@
+//
+//  BNRItemViewController2.h
+//  0526NMTel
+//
+//  Created by Zhixue Li on 6/2/15.
+//  Copyright (c) 2015 Zhixue Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "BNRDetailViewController.h"
+
+@interface BNRItemsViewController2 : UITableViewController
+
+@end
